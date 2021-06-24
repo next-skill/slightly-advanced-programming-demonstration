3528ce318f40b561c50777aac6b5011be9c56c23
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-06-24 21:32:56 +0900

    テストコードを共通化

---
+++ test/thread/thread_test.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require 'stringio'
+
+class ThreadTest < Minitest::Test
+  def test_thread_unsafe_variable_count
+    results = run_many_times './lib/thread/thread_unsafe_variable_count.rb'
+    assert_equal true, results.include?('1')
+    assert_equal true, results.include?('2')
+  end
+
+  def test_thread_safe_variable_count
+    results = run_many_times './lib/thread/thread_safe_variable_count.rb'
+    assert_equal false, results.include?('1')
+    assert_equal true, results.include?('2')
+  end
+
+  private
+
+  def run_many_times(file_name)
+    $stdout = StringIO.new
+
+    100.times do
+      load file_name
+    end
+
+    out = $stdout.string
+
+    $stdout = STDOUT
+
+    out.each_line(chomp: true)
+  end
+end
